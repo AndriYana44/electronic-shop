38d6b3de7d00b4667eae47d2c3f2d024be3fb143
unknown <BNI@DMA-29635-1.hq.bni.co.id>
2023-05-10 15:44:42 +0700

    add gudang page

---
+++ resources/views/gudang/index.blade.php
@@ -0,0 +1,18 @@
+@extends('produk.index')
+@section('navigation-produk')
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Handphone</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Aksesoris</a>
+        </li>
+    </ul>
+@stop
+@section('main-content')
+    <div class="item-menus row my-3">
+        <div class="col-12 d-flex justify-between">
+            
+        </div>
+    </div>
+@stop
