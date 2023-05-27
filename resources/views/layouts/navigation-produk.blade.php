3a7fafabf5ae8d487aaa7149f2c0d250ce1e2f4d
Andri Yana <andriyana@Andris-MacBook-Pro.local>
2023-05-27 14:26:55 +0700

    fixing aksesoris page

--- resources/views/layouts/navigation-produk.blade.php
+++ resources/views/layouts/navigation-produk.blade.php
@@ -3,13 +3,13 @@
         <i class="fa fa-mobile mr-2"></i>
         Handphone
     </a>
-    <a class="pulsa {{ str_contains(url()->current(), 'pulsa') ? 'active' : '' }}" href="{{ route('pulsa') }}">
-        <i class="fa fa-credit-card  mr-2"></i>
-        Pulsa
-    </a>
     <a class="aksesoris {{ str_contains(url()->current(), 'aksesoris') ? 'active' : '' }}" href="{{ route('aksesoris') }}">
         <i class="fa fa-gift  mr-2"></i>
         Aksesoris
+    </a>
+    <a class="pulsa {{ str_contains(url()->current(), 'pulsa') ? 'active' : '' }}" href="{{ route('pulsa') }}">
+        <i class="fa fa-credit-card  mr-2"></i>
+        Pulsa
     </a>
     <a class="servis {{ str_contains(url()->current(), 'servis') ? 'active' : '' }}" href="{{ route('servis') }}">
         <i class="fa fa-cogs mr-2"></i>
